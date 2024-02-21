9c114f55f5587163112a726d7d8417a1b9d3b639
YevhenKharko <yevhenkharko@gmail.com>
2024-02-21 20:33:31 +0200

    swiched browser to hash router

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import styled, { StyleSheetManager, ThemeProvider } from 'styled-components';
 
 import { Quiz } from './components/Quiz';
